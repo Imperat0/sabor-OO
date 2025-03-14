383e0d09569f8bcd3091f7354a89621f08393c97
Imperat0 <imperato06@gmail.com>
2025-03-14 14:23:47 -0300

    Herança da classe ItemCardapio nas demais

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_praca.receber_avaliacao('Daniel', 10)
-restaurante_praca.receber_avaliacao('Paulo', 8)
+bebida_suco = Bebida('Suco de Morango', 5.0, 'grande')
+prato_pao = Prato('Pao', 2.00, 'O melhor pão da cidade')
 
 def main():
-    Restaurante.listar_restaurantes()
+    pass
 
 if __name__ == '__main__':
     main()
